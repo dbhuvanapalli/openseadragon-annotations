10fc5462c874a5f18852df4822b4f6164b0ab675
Divya Bhuvanapalli <49796596+dbhuvanapalli@users.noreply.github.com>
2020-03-01 21:49:22 -0500

    extending tools

--- src/controls/Draw.js
+++ src/controls/Draw.js
@@ -1,17 +1,17 @@
 import Control from './Control';
-import drawGroupHover from '../../img/draw_grouphover.png';
-import drawHover from '../../img/draw_hover.png';
-import drawPressed from '../../img/draw_pressed.png';
-import drawRest from '../../img/draw_rest.png';
+import freehandGroupHover from '../../img/freehand_grouphover.png';
+import freehandHover from '../../img/freehand_hover.png';
+import freehandPressed from '../../img/freehand_pressed.png';
+import freehandRest from '../../img/freehand_rest.png';
 
-export default class Draw extends Control {
+export default class Freehand extends Control {
   constructor() {
     super({
-      Tooltip: 'Draw',
-      srcRest: drawRest,
-      srcGroup: drawGroupHover,
-      srcHover: drawHover,
-      srcDown: drawPressed,
+      Tooltip: 'Freehand',
+      srcRest: freehandRest,
+      srcGroup: freehandGroupHover,
+      srcHover: freehandHover,
+      srcDown: freehandPressed,
     });
   }
 }
