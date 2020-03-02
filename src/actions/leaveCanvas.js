b48f4d242250f22306a13dd935058dd54a2e924d
Divya Bhuvanapalli <49796596+dbhuvanapalli@users.noreply.github.com>
2020-03-01 22:24:31 -0500

    Update leaveCanvas.js

--- src/actions/leaveCanvas.js
+++ src/actions/leaveCanvas.js
@@ -6,16 +6,19 @@ export default function leaveCanvas(Dispatcher, Store) {
         type: 'ACTIVITY_UPDATE',
         inProgress: false,
       });
+      break;
     case 'RECTANGLE':
       Dispatcher.dispatch({
         type: 'ACTIVITY_UPDATE',
         inProgress: false,
       });
+      break;
     case 'CIRCLE':
       Dispatcher.dispatch({
         type: 'ACTIVITY_UPDATE',
         inProgress: false,
       });
+      break;
     case 'POINT':
       Dispatcher.dispatch({
         type: 'ACTIVITY_UPDATE',
