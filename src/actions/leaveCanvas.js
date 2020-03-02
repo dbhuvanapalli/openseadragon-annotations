2fb27c525f92b8ac51ed907703e705c44592a7e6
Divya Bhuvanapalli <49796596+dbhuvanapalli@users.noreply.github.com>
2020-03-01 22:21:24 -0500

    Update leaveCanvas.js

--- src/actions/leaveCanvas.js
+++ src/actions/leaveCanvas.js
@@ -1,7 +1,22 @@
 export default function leaveCanvas(Dispatcher, Store) {
   switch (Store.getMode()) {
 
-    case 'DRAW':
+    case 'FREEHAND':
+      Dispatcher.dispatch({
+        type: 'ACTIVITY_UPDATE',
+        inProgress: false,
+      });
+    case 'RECTANGLE':
+      Dispatcher.dispatch({
+        type: 'ACTIVITY_UPDATE',
+        inProgress: false,
+      });
+    case 'CIRCLE':
+      Dispatcher.dispatch({
+        type: 'ACTIVITY_UPDATE',
+        inProgress: false,
+      });
+    case 'POINT':
       Dispatcher.dispatch({
         type: 'ACTIVITY_UPDATE',
         inProgress: false,
